649377a0e7c65ce8599e760959324eafad424665
Lucas Rafael Gonçalves <lucas.goncalves@moovin.com.br>
2019-09-09 13:48:34 -0300

    Initial commit of my components without tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
+
+import Header from './components/header'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   )
 }
